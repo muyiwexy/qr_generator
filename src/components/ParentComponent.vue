468b4f8896d5ac610fa47c22a6da4ff501e8bb12
Femi-ige Muyiwa <84715593+muyiwexy@users.noreply.github.com>
2023-02-02 11:24:57 +0100

    Update ParentComponent.vue

--- src/components/ParentComponent.vue
+++ src/components/ParentComponent.vue
@@ -6,78 +6,38 @@
       }}</li>
     </ul>
     <div class="content" v-show="selectedItem === items[0]">
-      <ContentOne v-on:input-value="updateValue" />
     </div>
     <div class="content" v-show="selectedItem === items[1]">
-      <ContentTwo v-on:input-value2="updateValue2" v-on:input-value3="updateValue3" v-on:input-value4="updateValue4"
-        v-on:input-value5="updateValue5" />
     </div>
     <div class="content" v-show="selectedItem === items[2]">
-      <ContentThree @selected="setSelectedImage" />
     </div>
     <div class="content" v-show="selectedItem === items[3]">
-      <DisplayArea :InputData="InputData" :InputData2="InputData2" :InputData3="InputData3" :hexCode="hexCode"
-        :hexCode2="hexCode2" :selectedImage="selectedImage" />
     </div>
   </div>
 </template>
 
 <script>
-import ContentOne from './Content/ContentOne.vue';
-import ContentTwo from './Content/ContentTwo.vue';
-import ContentThree from './Content/ContentThree.vue';
-import DisplayArea from './Content/DisplayArea.vue';
-
 
 export default {
   name: 'HelloWorld',
   components: {
-    ContentOne,
-    ContentTwo,
-    ContentThree,
-    DisplayArea
   },
   data() {
     return {
-      InputData: '',
-      hexCode: '',
-      hexCode2: '',
-      InputData2: '',
-      InputData3: '',
-      selectedImage: '',
-      items: [
-        { id: 1, title: 'ENTER CONTENT', content: 'This is the content of item 1' },
-        { id: 2, title: 'CUSTOMIZE', content: 'This is the content of item 2' },
-        { id: 3, title: 'ADD LOGO', content: 'This is the content of item 3' },
-        { id: 3, title: 'DISPLAY', content: 'This is the content of item 4' },
+      items: [        
+        { title: 'ENTER CONTENT' },
+        {title: 'CUSTOMIZE'},
+        {title: 'ADD LOGO', },
+        {title: 'DISPLAY' },
       ],
       selectedItem: null
     }
   },
   methods: {
-    updateValue(value) {
-      this.InputData = value
-    },
-    updateValue2(value) {
-      this.hexCode = value
-    },
-    updateValue3(value) {
-      this.hexCode2 = value
-    },
-    updateValue4(value) {
-      this.InputData2 = value
-    },
-    updateValue5(value) {
-      this.InputData3 = value
-    },
-    setSelectedImage(selectedImage) {
-      this.selectedImage = selectedImage;
-    },
     showContent(item) {
       this.selectedItem = item
     },
   },
-  emits: ['Selected']
 }
 </script>
 
